e180fafc0133ca1d90d0b6712d6f4bc993e738ea
Ch-Praneeth1 <chpraneethvardhanreddy143@gmail.com>
2024-02-16 08:43:14 +0530

    Product Schema

--- model/Product.js
+++ model/Product.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
-const {Schema} = mongoose;
-
+const { Schema } = mongoose;
 
 const productSchema = new Schema({
-
-});
-
-exports.Product = mongoose.model('Product',productSchema);
+    title: {type: String, required: true, unique: true} ,
+    description: String,
+    price: {type: Number, min:[0,'wrong price'],required: true},
+    discountPercentage: {type: Number, min:[0,'wrong min discount'], max:[99,'wrong max discount']},
+    rating: {type: Number, min:[0,'wrong min rating'], max:[5,'wrong max rating'], default:0},
+    brand: {type: String,required: true},
+    category: {type: String, required: true},
+    thumbnail: {type: String, required: true},
+    images: [ String ],
+    deleted: {type: Boolean, default:false}
+  });
+  
+exports.Product = mongoose.model('Product', productSchema);
