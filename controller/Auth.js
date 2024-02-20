7512fcc59206980b9d840c9b07f39fd98dad5500
Ch-Praneeth1 <chpraneethvardhanreddy143@gmail.com>
2024-02-20 12:10:20 +0530

    Passport js authentication and Password encryption

--- controller/Auth.js
+++ controller/Auth.js
@@ -1,29 +1,25 @@
 const { User } = require("../model/User");
+const crypto = require('crypto');
 
-exports.createUser= async (req,res) => {
-    const user = new User(req.body);
+
+exports.createUser = async (req,res) => {
     try{
+        const salt = crypto.randomBytes(16);
+        crypto.pbkdf2(req.body.password, salt, 310000, 32, 'sha256', async function(err, hashedPassword){
+        const user = new User({...req.body,password:hashedPassword, salt});
         const doc = await user.save();
         res.status(201).json(doc);
-    }catch (err) {
+        })
+    } catch (err) {
         res.status(400).json(err);
     }
 };
 
-exports.loginUser= async (req,res) => {
-    //TODO: We need a strong password authentication 
-    try{
-        const user = await User.findOne({email:req.body.email}).exec();
-        if(!user){
-            res.status(401).json({message: 'no such user email'});
-        }
-        else if(user.password === req.body.password){
-            //TODO: need to make addresses independent from login 
-            res.status(200).json({id:user.id, name:user.name, email:user.email, addresses:user.addresses, role:user.role});
-        } else {
-            res.status(401).json({message: 'invalid credentials'});
-        }
-    }catch (err) {
-        res.status(400).json(err);
-    }
+exports.loginUser = async (req,res) => {
+    //DONE: We need a strong password authentication 
+   res.json(req.user);
 };
+
+exports.checkUser = async (req,res) => {
+    res.json(req.user);
+}
--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const { createProduct } = require('./controller/Product');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const crypto = require('crypto');
+
 const server = express();
 const productsRouter = require('./routes/Product')
 const categoriesRouter = require('./routes/Category')
@@ -10,12 +14,20 @@ const usersRouter = require('./routes/Users');
 const authRouter = require('./routes/Auth');
 const cartRouter = require('./routes/Cart');
 const ordersRouter = require('./routes/Orders');
+const { User } = require('./model/User');
 
 
 //middlewares
+server.use(session({
+    secret: 'keyboard cat',
+    resave: false, // don't save session if unmodified
+    saveUninitialized: false, // don't create session until something stored
+    
+  }));
+server.use(passport.authenticate('session'));
 server.use(cors());
 server.use(express.json());//helps to parse req.body
-server.use('/products',productsRouter.router);
+server.use('/products',isAuth, productsRouter.router);
 server.use('/categories',categoriesRouter.router);
 server.use('/brands',brandsRouter.router);
 server.use('/users',usersRouter.router);
@@ -23,6 +35,40 @@ server.use('/auth',authRouter.router);
 server.use('/cart',cartRouter.router);
 server.use('/orders',ordersRouter.router);
 
+// Passport Strategies
+passport.use(new LocalStrategy( async function(username, password, done) {
+        try{
+            const user = await User.findOne({email:username}).exec();
+            if(!user){
+                done(null, false, {message: 'invalid credentials'})
+            }
+            crypto.pbkdf2(password, user.salt, 310000, 32, 'sha256', async function(err, hashedPassword){
+                //TODO: this is just temporary, we will use JWT
+                if (!crypto.timingSafeEqual(user.password, hashedPassword)){
+                    return done(null, false, {message: 'invalid credentials'});                
+                } else {
+                    done(null, {id:user.id, name:user.name, email:user.email, addresses:user.addresses, role:user.role});
+
+                }
+            })
+        }catch (err) {
+            done(err);
+        }
+    }
+));
+
+// This creates a session variable req.user on being called from callbacks
+passport.serializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, {id:user.id, role:user.role});
+    });
+  });
+// This changes a session variable req.user when called from  autheorised request
+  passport.deserializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, user);
+    });
+  });
 
 main().catch(err => console.log(err));
 async function main() {
@@ -34,7 +80,13 @@ server.get('/', (req,res) => {
     res.json({status:"success"})
 })
 
-
+function isAuth(req,res,done){
+    if(req.user){
+        done()
+    }else{
+        res.send(401)
+    }
+}
 server.listen(8080, ()=>{
     console.log("server started")
 });
